700994dcdfed9711a25fe3d51aa4f5af44742c7f
Malcolm <redwindmh@gmail.com>
2023-07-27 19:46:36 +0900

    Made headway on navbar, linked Tailwind with Vite

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,17 @@ const Navbar = () => {
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
-        <Link to="/" className="flex items-center gap-2" onClick={()=> { setActive("")} }>
-
+        <Link to="/" className="flex items-center gap-2" onClick={()=> { setActive(""); window.scrollTo(0,0)} }>
+          <img src={logo} alt="HTlogo" className='object-contain w-12 h-12' />
+          <p className='text-white text-[18px font-bold cursor-pointer hover:text-red-600'>Malcolm <span>| Portfolio</span></p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li key={link.id} className={`${active===link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={()=>setActive(link.title)}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
