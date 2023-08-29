c32ec81e81d2960822797d12eecf67ff6051b5ee
Malcolm <redwindmh@gmail.com>
2023-08-30 08:37:23 +0900

    Began work on 3D hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,11 +14,16 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi! I'm <span className="text-[red]">Malcolm</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I make this shit look easy.
+            I'm your friendly neighborhood technomancer.
           </p>
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center"></div>
+        </a>
+      </div>
     </section>
   )
 }
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Html, useProgress } from '@react-three/drei'
+import { progress } from 'framer-motion'
 
 const Loader = () => {
+  const { progress } = useProgress()
+
   return (
-    <div>Loader</div>
+    <Html>
+      <span className="canvas-load"></span>
+      <p className='text-sm text-white font-extrabold mt-10 text-center'>Loading {progress.toFixed()}%</p>
+    </Html>
   )
 }
 
