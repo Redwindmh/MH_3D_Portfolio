211c3f90c82bab1bf38be028cbd4b9c976143154
Malcolm Hendricks <redwindmh@gmail.com>
2023-08-30 17:46:21 +0900

    Began work on About section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,7 +11,7 @@ const Computers = ( isMobile ) => {
       <hemisphereLight intensity={3} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight position={[-20,50,10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
-      <primitive object={computer.scene} scale={ isMobile ? 0.7 : 0.75} position={[0,-3.25,-1.5]} rotation={[-0.01,-0.2,-0.1]} />
+      <primitive object={computer.scene} scale={ isMobile ? 0.7 : 0.75} position={ isMobile ? [0,-3,-2.2] : [0,-3.25,-1.5]} rotation={[0,-0.2,-0.1]} />
     </mesh>
   )
 }
@@ -20,7 +20,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false)
 
   useEffect(()=> {
-    const mediaQuery = window.matchMedia('max-width:500')
+    const mediaQuery = window.matchMedia('(max-width:500px)')
 
     setIsMobile(mediaQuery.matches)
 
@@ -31,7 +31,7 @@ const ComputersCanvas = () => {
     mediaQuery.addEventListener('change', handleMediaQueryChange)
 
     return mediaQuery.removeEventListener('change',handleMediaQueryChange, [])
-  })
+  },[])
 
   return (
     <Canvas frameLoop="demand" shadows camera={{position:[20,3,5], fov: 25}} gl={{preserveDrawingBuffer:true}}>
