e60c98f9498105f9673aaf5494988af05928154f
Malcolm Hendricks <redwindmh@gmail.com>
2023-09-04 13:16:54 +0900

    Started work on Portfolio section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import { motion } from 'framer-motion'
+import 'react-vertical-timeline-component/style.min.css'
+import { styles } from '../styles'
+import { experiences } from '../constants'
+import { SectionWrapper } from '../hoc'
+import { textVariant } from '../utils/motion'
+
+const ExperienceCard = ({experience}) => {
+  <VerticalTimelineElement contentStyle={{background:'#1d1836',color:'#fff'}} contentArrowStyle={{borderRight:'7px solid #232631'}} date={experience.date} iconStyle={{background:experience.iconBg}}>
+    {experience.date}
+  </VerticalTimelineElement>
+}
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Here are some of my projects</p>
+        <h2 className={styles.sectionHeadText}>Portfolio</h2>
+      </motion.div>
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience,index)=>(<ExperienceCard key={index} experience={experience} />))}
+        </VerticalTimeline>
+      </div>
+    </>
   )
 }
 
-export default Experience
+export default SectionWrapper(Experience, "work")
