04805396ba48ac4a0232b12e7d23a8caa95b7530
Malcolm Hendricks <redwindmh@gmail.com>
2023-09-25 23:10:28 +0900

    Continued work on Portfolio section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { github } from '../assets'
 import { SectionWrapper } from '../hoc'
@@ -9,7 +10,18 @@ import { fadeIn, textVariant } from '../utils/motion'
 const Works = () => {
   return (
     <>
-      
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Portfolio</p>
+        <h2 className={styles.sectionHeadText}>Projects</h2>
+      </motion.div>
+      <div className="w-full flex">
+        <motion.p variants={fadeIn("","",0.1,1)} className='mt-3 text-secondary text-[17px] max-w-3xl leading-30px' >
+          Here are some projects I've done that I feel showcase what I've studied so far.
+        </motion.p>
+      </div>
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project,index)=>(<ProjectCard key={`project-${index}`} index={index}/> ))}
+      </div>
     </>
   )
 }
