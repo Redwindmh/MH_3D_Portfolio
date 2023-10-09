e8e64faa3c50303d6a415b2becf5ea16acfd0dbb
Malcolm <redwindmh@gmail.com>
2023-10-09 23:24:35 +0900

    Finished Portfolio section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -13,7 +13,17 @@ const ProjectCard = ({ index,name,description,tags,image,source_code_link}) => {
       <Tilt options={{max:45,scale:1,speed:450}} className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
         <div className="relative w-full h-[230px]">
           <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl'/>
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div className="black-gradient w-10 h-10 rounded-full flex items-center justify-center cursor-pointer" onClick={()=>window.open(source_code_link, "_blank")}>
+              <img src={github} alt="github_link" className="w-1/2 h-1/2 object-contain" />
+            </div>
+          </div>
         </div>
+        <div className="mt-5">
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p>{description}</p>
+        </div>
+        <div className="mt-4 flex flex-wrap gap-2">{tags.map((tag) => (<p key={tag.name} className={`text-[14px] ${tag.color}`} >#{tag.name}</p>))}</div>
       </Tilt>
     </motion.div>
   )
@@ -32,7 +42,7 @@ const Works = () => {
         </motion.p>
       </div>
       <div className="mt-20 flex flex-wrap gap-7">
-        {projects.map((project,index)=>(<ProjectCard key={`project-${index}`} index={index}/> ))}
+        {projects.map((project,index)=>(<ProjectCard key={`project-${index}`} index={index} {...project}/> ))}
       </div>
     </>
   )
