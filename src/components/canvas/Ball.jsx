d343812003e7d80047ccfa9991bef5b571818db0
Malcolm <redwindmh@gmail.com>
2023-12-03 16:22:38 +0900

    Trying to fix modile white screen bug

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -7,7 +7,7 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl])
 
   return(
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={5}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0,0,0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
@@ -21,7 +21,7 @@ const Ball = (props) => {
 
 const BallCanvas = ({icon}) => {
   return (
-    <Canvas frameloop='demand' gl={{ preserveDrawingBuffer: true }}>
+    <Canvas gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
