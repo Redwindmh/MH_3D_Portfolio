bc7743960af97b2dcbc5e0889b03eaae9f28dff9
Malcolm <redwindmh@gmail.com>
2023-12-15 11:20:06 +0900

    Okay, this I think I really switched

---
+++ src/assets/keys.js
@@ -0,0 +1,6 @@
+export const templateID="template_5oz5od8"
+export const serviceID="service_3e5q9sk"
+export const emailJSKey="BxYR4LWs4_LqJ7wBv"
+
+export const myName="Malcolm"
+export const myEmail="malhendricks"
