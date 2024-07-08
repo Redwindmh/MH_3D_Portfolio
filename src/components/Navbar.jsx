f7ae3965c66213a33d4badfe78c93a2f46c3a630
Malcolm <redwindmh@gmail.com>
2024-07-08 23:09:50 +0900

    Fixed logo link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
-        <Link to="/" className="flex items-center gap-2" onMouseDown={() => { setActive(""); window.scrollTo(0, 0) }}>
+        <Link to="/MH_3D_Portfolio/" className="flex items-center gap-2" onMouseDown={() => { setActive(""); window.scrollTo(0, 0) }}>
           <img src={logo} alt="HTlogo" className='object-contain w-12 h-12' />
           <p className='text-white text-[18px] font-bold cursor-pointer hover:text-red-600'>Malcolm&nbsp;<span>|&nbsp;Portfolio</span></p>
         </Link>
